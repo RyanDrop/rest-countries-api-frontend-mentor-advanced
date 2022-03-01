61c535dded632328babec3e3fb863d75deec4ee7
RyanDrop <ryangithub7509@gmail.com>
2022-02-28 22:34:48 -0300

    ✨ feat(component): country card

---
+++ src/app/components/country-card/country-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-country-card',
+  templateUrl: './country-card.component.html',
+  styleUrls: ['./country-card.component.scss'],
+})
+export class CountryCardComponent {
+  @Input() country: any;
+}
