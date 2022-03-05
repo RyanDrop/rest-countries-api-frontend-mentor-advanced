9e05940520d277c2fa73d7fae97c46ec603c7cdc
RyanDrop <ryangithub7509@gmail.com>
2022-03-04 23:12:19 -0300

    ✨ feat(rest countries api service): method getCountriesByLocalStorage and new behaviorSubject

--- src/app/services/rest-countries-api/rest-countries-api.service.ts
+++ src/app/services/rest-countries-api/rest-countries-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Country } from 'src/models/country.models';
 import { LocalStorageService } from '../local-storage/local-storage.service';
 
@@ -7,10 +8,8 @@ import { LocalStorageService } from '../local-storage/local-storage.service';
   providedIn: 'root',
 })
 export class RestCountriesApiService {
-  private API_URL = 'https://restcountries.com/v2/';
-  private filters: Array<Country> = [];
-  countries?: Array<Country>;
-  researchedCountries?: Array<Country>;
+  private REST_COUNTRIES_API_URL = 'https://restcountries.com/v2/';
+  allCountries$$ = new BehaviorSubject<Country[]>([]);
 
   constructor(
     private http: HttpClient,
@@ -25,9 +24,14 @@ export class RestCountriesApiService {
     this.http
       .get(`${this.REST_COUNTRIES_API_URL}all`)
       .subscribe((countries) => {
-      this.localStorage.set('countries', countries);
+        this.localStorage.set('countries', countries);
         this.allCountries$$.next(this.localStorage.get('countries'));
-    });
+      });
+  }
+
+  getCountriesByLocalStorage() {
+    const storageCountries: Country[] = this.localStorage.get('countries');
+    return storageCountries;
   }
 
   getCountryByName(name: string) {
@@ -43,36 +47,6 @@ export class RestCountriesApiService {
     const country = storageCountries.find((country: { alpha3Code: string }) => {
       return country.alpha3Code.includes(code);
     });
-
     return country.name;
-  }
-
-  searchCountries(search: string) {
-    const getCountries =
-      this.filters.length === 0
-        ? this.localStorage.get('countries')
-        : this.filters;
-
-    const filterCountries = getCountries.filter((country: { name: string }) => {
-      return country.name.toLowerCase().includes(search.toLowerCase());
-    });
-
-    return (this.researchedCountries = filterCountries);
-  }
-
-  filterCountriesByRegion(region: string) {
-    const getCountries = this.localStorage.get('countries');
-    if (region === 'All regions') {
-      this.filters = getCountries;
-      return (this.countries = getCountries);
-    }
-
-    const filterCountries = getCountries.filter(
-      (country: { region: string }) => {
-        return country.region.toLowerCase().includes(region.toLowerCase());
-      }
-    );
-    this.filters = filterCountries;
-    return (this.countries = filterCountries);
   }
 }
