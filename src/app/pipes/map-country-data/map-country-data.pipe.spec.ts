55777a2aeb4bc723d994d7c35d7668cd8f184409
RyanDrop <ryangithub7509@gmail.com>
2022-03-13 18:37:36 -0300

    🗑️ revert(pipe): remove pipe map-country-data

--- src/app/pipes/map-country-data/map-country-data.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MapCountryDataPipe } from './map-country-data.pipe';
-
-describe('MapCountryDataPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MapCountryDataPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
